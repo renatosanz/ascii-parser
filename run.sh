7215417089172bea286ebe0fdc00889a7418def8
renatosanz <renato.sanchez3103@gmail.com>
2025-06-11 13:50:01 -0600

    removing stufff

--- run.sh
+++
@@ -1,3 +0,0 @@
- meson setup --wipe build && meson compile -C build                                                                       
- build/ascii-parser -i test.jpg   -o renato.txt -ra                                                                   
- xdg-open renato.txt.png     
